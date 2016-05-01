585ae250db1627bbe18b04dda0dcb1eb9247f998
Tyom Semonov <inspiro@gmail.com>
2016-05-01 01:41:10 +0100

    Configure CSS Modules naming

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ var config = {
     loaders: [
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&sourceMap!postcss')
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]-[local]&sourceMap!postcss')
       },
       {
         test: /\.js$/,
